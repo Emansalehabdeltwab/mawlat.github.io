e1c3f7727906491a5ee673b4c8350bbe9aaf2460
emansalehmohammad <emansalehmohammad9@gmail.com>
2020-06-02 18:12:11 +0200

    fm

---
+++ js/demo.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $("#btntop").fadeIn();
+        }
+        else {
+            $("#btntop").fadeOut();
+        }
+    });
+    $("#btntop").click(function () {
+        $("html,body").animate({
+            scrollTop: 0}, 800)
+    });
+    
+    
+});
